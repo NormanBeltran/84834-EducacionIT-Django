f6eb9a217194ad8d48b6084cf1d6bc937b06cd4f
NormanBeltran <josenormanbeltran@gmail.com>
2026-06-01 20:54:59 -0300

    clase_2

--- mip/miapp/urls.py
+++ mip/miapp/urls.py
@@ -2,7 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("",            views.index,        name="index"),  
-    path("acerca",      views.acerca,       name="acerca"),  
-    path("contacto",    views.contacto,     name="contacto"),  
+    path("",            views.index,                name="index"),  
+    path("acerca",      views.acerca,               name="acerca"),  
+    path("contacto",    views.contacto,             name="contacto"),  
+    path("cursos",      views.consultar_cursos,     name="cursos"),  
+    path("aero",        views.aeropuertos,          name="aero"),  
+    path("aero_api",    views.aero_api,             name="aero_api"),  
+
+    path("bienvenido",  views.bienvenido,           name="bienvenido"),  
+    path("bienvenido2", views.bienvenido2,          name="bienvenido2"),  
 ]
--- mip/miapp/views.py
+++ mip/miapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+import sqlite3
 
 # Create your views here.
 def index(request):
@@ -14,3 +16,109 @@ def contacto(request):
                         <h1>Contacto: Norman Beltran</h1>
                         </html>
                         """)
+
+# Lectura de BD y envío de Información al cliente
+
+def consultar_cursos(request):
+    conn = sqlite3.connect('curso.db')
+    
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT id, nombre, inscriptos from cursos;')
+
+    html = """
+    <html>
+    <title>Lista de Cursos</title>
+    <table style="border:1px solid">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Inscriptos</th>
+        </tr>
+    </thead>
+    """
+
+    for (id, nombre, inscriptos) in cursor.fetchall():
+        html += "<tr>"
+        html += "<td>" + str(id) + "</td>" 
+        html += "<td>" + nombre + "</td>" 
+        html += "<td>" + str(inscriptos) + "</td>" 
+        html += "</tr>"
+
+    html += """
+    </table>
+    </html>
+    """
+    conn.close()
+    return HttpResponse(html)
+
+# Lectura de Archivos (CSV) para enviar formateado al cliente
+
+def aeropuertos(request):
+    html = """
+    <html>
+    <title>Lista de Aeropuertos</title>
+    <table style="border:1px solid">
+    <thead>
+        <tr>
+            <th>Aeropuerto</th>
+            <th>Ciudad</th>
+            <th>País</th>
+            <th>Código</th>
+        </tr>
+    </thead
+    """
+
+    with open("aeropuertos.csv", "r", encoding="utf-8") as file:
+        for linea in file:
+            datos = linea.split(',')
+            html += "<tr>"
+            html += "<td>" + datos[1].replace('"', '')  + "</td>"
+            html += "<td>" + datos[2].replace('"', '')  + "</td>"
+            html += "<td>" + datos[3].replace('"', '')  + "</td>"
+            html += "<td>" + datos[4].replace('"', '')  + "</td>"
+            html += "</tr>"
+
+    html += """
+    </table>
+    </html>
+    """
+    return HttpResponse(html)
+
+#  APIS con Django 
+
+def aero_api(request):
+    aeropuertos = []
+
+    with open("aeropuertos.csv", "r", encoding="utf-8") as file:
+        for linea in file:
+            datos = linea.split(',')
+
+            a_nombre = datos[1].replace('"', '')
+            a_ciudad = datos[2].replace('"', '')
+            a_pais   = datos[3].replace('"', '')
+            a_codigo = datos[4].replace('"', '')
+
+            aeropuerto = {
+                "nombre": a_nombre,
+                "ciudad": a_ciudad,
+                "pais"  : a_pais,
+                "codigo": a_codigo,
+            }
+            aeropuertos.append(aeropuerto)
+    return JsonResponse(aeropuertos, safe=False) 
+
+# Separa código HTML del código Django / Python
+
+def bienvenido(request):
+    with open("miapp/templates/miapp/bienvenido.html", "r", encoding="utf-8") as file:
+        html = file.read()
+    return HttpResponse(html)
+
+def bienvenido2(request):
+    pagina = "miapp/bienvenido2.html"  # Por default Django busca la carpeta /templates dentro de la aplicacion
+    ctx = { "nombre": "Curso De Django (Desarrollo Web)", "comision": 84834, "dias": "Lunes y Jueves",
+           "profesor": "Norman Beltran"}
+    return render(request, pagina, ctx)
+    
